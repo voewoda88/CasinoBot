324282e0d05a1d4f3f240cbc491bb678378965dd
Ivan Bagay <voewoda88@gmail.com>
2024-04-23 01:30:28 +0300

    feat: added config reader

---
+++ casinobot/config_reader.py
@@ -0,0 +1,19 @@
+from enum import Enum
+import os
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    bot_token: SecretStr
+    fsm_mode: str
+    bot_language: str
+    starting_points: int = 50
+    send_gameover_sticker: bool = False
+    throttle_time_spin: int = 2
+    throttle_time_other: int = 1
+
+    model_config = SettingsConfigDict(env_file='config.env', env_file_encoding='utf-8')
