8eddacfd18fafa2f6f64d69f6ead803894361188
Ivan Bagay <voewoda88@gmail.com>
2024-04-23 01:33:10 +0300

    feat: added handler for standart user commands

---
+++ casinobot/Handlers/userHandlers.py
@@ -0,0 +1,26 @@
+from aiogram.dispatcher.router import Router
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
+from fluent.runtime import FluentLocalization
+
+from config_reader import Settings
+from keyboards import get_keyboard
+
+flags = {"throttling_key": "default"}
+router = Router()
+
+@router.message(Command("start"), flags=flags)
+async def start(message: Message, state: FSMContext, l10n: FluentLocalization, config: Settings):
+    start_text = l10n.format_value("start-text", {"points": config.starting_points})
+
+    await state.update_data(score=config.starting_points)
+    await message.answer(start_text, reply_markup=get_keyboard(l10n))
+
+@router.message(Command("stop"), flags=flags)
+async def stop(message: Message, l10n: FluentLocalization):
+    await message.answer(l10n.format_value("stop-text"), reply_markup=ReplyKeyboardRemove())
+
+@router.message(Command("help"), flags=flags)
+async def help(message: Message, l10n: FluentLocalization):
+    await message.answer(l10n.format_value("help-text"), disable_web_page_preview=True)
