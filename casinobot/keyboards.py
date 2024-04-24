b1b42cbe10e1fb0893d6f3981f8bcc122c44af92
Ivan Bagay <voewoda88@gmail.com>
2024-04-24 04:02:13 +0300

    update: added keyboards

--- casinobot/keyboards.py
+++ casinobot/keyboards.py
@@ -4,9 +4,22 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from fluent.runtime import FluentLocalization
 
 @cache
-def get_keyboard(l10n: FluentLocalization):
+def get_games_keyboard(l10n: FluentLocalization):
+    first_row = [
+        KeyboardButton(text=l10n.format_value("spin-button-text")),
+        KeyboardButton(text=l10n.format_value("black-jack-text")),
+    ]
+
+    second_row = [
+        KeyboardButton(text=l10n.format_value("change-bet-text"))
+    ]
+
+    return ReplyKeyboardMarkup(keyboard=[first_row, second_row], resize_keyboard=True)
+
+@cache
+def get_main_menu(l10n: FluentLocalization):
     keyboard = [
-        [KeyboardButton(text=l10n.format_value("spin-button-text"))]
+        [KeyboardButton(text=l10n.format_value("start-game-text"))]
     ]
 
     return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
