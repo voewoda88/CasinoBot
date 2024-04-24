862890902b5c776ffc9e8919c2ccdb5cf236a5cd
Ivan Bagay <voewoda88@gmail.com>
2024-04-24 15:47:52 +0300

    update: added handlers

--- casinobot/Handlers/spinHandler.py
+++ casinobot/Handlers/spinHandler.py
@@ -6,16 +6,24 @@ from aiogram.enums.dice_emoji import DiceEmoji
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 from fluent.runtime import FluentLocalization
 
-from Filters.filters import SpinTextFilter
+from Filters.filters import SpinTextFilter, RepeatFilter, BackFilter
 from config_reader import Settings
-from keyboards import get_games_keyboard
+from keyboards import get_games_keyboard, get_return_menu
 from dice_check import get_score_change, get_combo_text
 
-flags = {"throttling_key": "spin"}
+flags = {"throttling_key": "game"}
 router = Router()
+
+@router.message(RepeatFilter(), flags=flags)
+async def repeat_spin(message: Message, state: FSMContext, l10n: FluentLocalization, config: Settings):
+    await spin(message, state, l10n, config)
+
+@router.message(BackFilter(), flags=flags)
+async def back_spin(message: Message, l10n: FluentLocalization):
+    await message.answer(l10n.format_value("choice-game-text"), reply_markup=get_games_keyboard(l10n))
 
 @router.message(Command("spin"), flags=flags)
 @router.message(SpinTextFilter(), flags=flags)
@@ -36,7 +44,7 @@ async def spin(message: Message, state: FSMContext, l10n: FluentLocalization, co
         await message.answer(l10n.format_value("bet-is-greater-than-balance"))
         return
 
-    msg = await message.answer_dice(emoji=DiceEmoji.SLOT_MACHINE, reply_markup=get_games_keyboard(l10n))
+    msg = await message.answer_dice(emoji=DiceEmoji.SLOT_MACHINE, reply_markup=ReplyKeyboardRemove())
 
     score_change = get_score_change(msg.dice.value, bet_value)
 
@@ -58,5 +66,5 @@ async def spin(message: Message, state: FSMContext, l10n: FluentLocalization, co
                 "result_text": win_or_lose_text,
                 "new_score": new_score
             }
-        )
+        ), reply_markup=get_return_menu(l10n)
     )
