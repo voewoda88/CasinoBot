fdb40fb29919eb6ccd626498d92069d4abaced6f
Ivan Bagay <voewoda88@gmail.com>
2024-04-24 15:47:18 +0300

    update: added filters

--- casinobot/Filters/filters.py
+++ casinobot/Filters/filters.py
@@ -13,3 +13,19 @@ class GamesFilter(BaseFilter):
 class BetFilter(BaseFilter):
     async def __call__(self, message: Message, l10n: FluentLocalization) -> bool:
         return message.text == l10n.format_value("change-bet-text")
+
+class RepeatFilter(BaseFilter):
+    async def __call__(self, message: Message, l10n: FluentLocalization) -> bool:
+        return message.text == l10n.format_value("repeat-game-button-text")
+
+class BackFilter(BaseFilter):
+    async def __call__(self, message: Message, l10n: FluentLocalization) -> bool:
+        return message.text == l10n.format_value("back-button-text")
+
+class CheckBalanceFilter(BaseFilter):
+    async def __call__(self, message: Message, l10n: FluentLocalization) -> bool:
+        return message.text == l10n.format_value("balance-info-button")
+
+class BlackJackTextFilter(BaseFilter):
+    async def __call__(self, message: Message, l10n: FluentLocalization) -> bool:
+        return message.text == l10n.format_value("blackjack-text")
